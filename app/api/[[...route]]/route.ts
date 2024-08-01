57f1b031ced6ba14709a7c2a354d7bb65162cd0e
haikaljs <haikaljavascript@gmail.com>
2024-08-01 21:46:52 +0800

    hono.js with  middleware clerk auth

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,52 +1,28 @@
-
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import { z } from "zod";
 import { zValidator } from "@hono/zod-validator";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app
-  .get("/hello", (c) => {
+app.get("/hello", clerkMiddleware(), (c) => {
+
+  const auth = getAuth(c);
+  
+  if (!auth?.userId) {
     return c.json({
-      message: "Hello Next.js!",
+      error: "Unauthorized",
     });
-  })
-  .get("/hello/:test",
-    zValidator("param", z.object({
-      test: z.string()
-    })),
-    
-    (c) => {
-    const {test} = c.req.valid("param");
+  }
 
-    return c.json({
-      message: "Hello test hono",
-      test: test,
-    })
- 
-  })
-  .post("/creat/postId",
-    zValidator("json", z.object({
-      name: z.string(),
-      userId: z.number()
-    })),
-    zValidator("param", z.object({
-      postId: z.string(),
-     
-    })),
-     (c) => {
-      const {name, userId} = c.req.valid("json")
-      const {postId} = c.req.valid("param")
-    return c.json({
-      message: "Just a test zod with hono.js",
-      name,
-      postId
-
-    })
-  })
+  return c.json({
+    message: "Hello Next.js!",
+    userId: auth.userId
+  });
+});
 
 export const GET = handle(app);
 export const POST = handle(app);
